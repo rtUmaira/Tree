0d4421af7cf5d83042248971f03579af7d6ff929
umairasajjad <umairasajjad@gmail.com>
2016-10-13 07:23:24 +0500

    updated

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-	//$(document).ready(function () {
-	    
-	// });
-
 	$('#myTree').tree();
 
 	function toggle(element) {
+		console.log('----------');
 		var folder = $(element).closest("li")[0];
 		// find the folder's concerned img 
 		var img_id = folder.children[0].children[1].id;
@@ -21,24 +18,40 @@
 		var size_li = $("#"+ul_id).children("li").size();
 		//console.log('size: ', size_li);
 	    x = 3;
-	    $("#"+ul_id).children(" li:lt("+x+")").show();
+	    $("#"+ul_id).children(" li:lt("+x+")").css( "display", "block");
 	    $('#'+load_id).click(function () {
 	        x = (x+3 <= size_li) ? x+3 : size_li;
 	        // console.log('x: ', x);
-	        $("#"+ul_id).children(" li:lt("+x+")").show();
+	        $("#"+ul_id).children(" li:lt("+x+")").css( "display", "block");
+	        //console.log('3: ', $("#"+ul_id).children("li"));
+
 	    });
+	    // console.log('3: ', $("#"+ul_id).children("li"));
+	    //console.log($("#folder1").css('display'));
 	}
 
 	function sort(element) {
 		// get children of branch
-		var id = $(element).closest("li")[0].children[1].id;
-		console.log('id', id);
+		// console.log('xxxxxxxxxxxx');
 
-		$('#'+id).toggleClass('checked');
+		var items = [];
+		var children = $(element).closest("li").children("ul").children("li");
 
-		var items = $(element).closest("li").children("ul").children("li");
+		//console.log($('#item1').css('display'));
+		// console.log('children: ', children);
+		for (var i = 0; i < children.length; i++) {
+		  	var child = children[i];
+		  	// console.log('child: ', child,  $('#' + child.id).css('display') );
+		  	if ( $('#'+child.id).css("display") === "block")
+		  	{
+		  		items.push(child);
+		  	}
+		}
 		// console.log(items[1].id.indexOf("folder"));
 		
+		var id = $(element).closest("li")[0].children[1].id;
+
+		$('#'+id).toggleClass('checked');
 		items.sort(function(a,b){
 			var keyA;
 			var keyB;
